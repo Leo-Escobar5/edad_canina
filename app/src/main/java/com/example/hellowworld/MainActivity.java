f66fd231ecb4f697769d7ef8b811e1452ffaa7d2
Leonardo Alberto Escobar Paredes <20000453@alumnos.utleon.edu.mx>
2023-03-22 12:56:29 -0600

    Aplicación de edad canina terminada y con icono de la aplicación

--- app/src/main/java/com/example/hellowworld/MainActivity.java
+++ app/src/main/java/com/example/hellowworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hellowworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,26 +11,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.hellowworld.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        EditText ageEdit = findViewById(R.id.age_edit);
-        TextView resultText = findViewById(R.id.result_text);
-        Button button = findViewById(R.id.button);
 
         Log.i("MainActivity","Activity created");
 
-        button.setOnClickListener(v -> {
-            String age = ageEdit.getText().toString();
+        binding.button.setOnClickListener(v -> {
+            String age = binding.ageEdit.getText().toString();
             if (!age.isEmpty()) {
                 int ageInt = Integer.parseInt(age);
             int result = ageInt * 7;
             String resultString = getString(R.string.result_format,result);
-            resultText.setText(resultString);
+                binding.resultText.setText(resultString);
             } else{
                 Log.e("MainActivity","Age field is empty");
                 Toast.makeText(this,getString(R.string.you_have_to_insert_an_age),Toast.LENGTH_SHORT).show();
